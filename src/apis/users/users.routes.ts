b47c09c2d61096b5df1091d5ecee9e39664f68c6
Sayuri Cero <Hisaalqaoud@gmail.com>
2025-09-07 19:41:57 +0300

    most of tast is done

--- src/apis/users/users.routes.ts
+++ src/apis/users/users.routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { authorize } from '../../middlewares/JWT';
 import { RequestHandler } from 'express';
 
 const router = express.Router();
@@ -7,6 +8,6 @@ import { signup, signin, getUsers } from './users.controllers';
 
 router.post('/signup', signup as RequestHandler);
 router.post('/signin', signin as RequestHandler);
-router.get('/users', getUsers as RequestHandler);
+router.get('/users', authorize, getUsers as RequestHandler);
 
 export default router;
